800674654728012a30e05701d233dbec264dbb8a
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-07-28 21:30:10 +0500

    fixed the newsletter form, removed the action and method for now, and it is working normal.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -129,14 +129,14 @@
                 </p>
 
                 <form class="mt-4">
-                    @csrf
+                    {{-- @csrf --}}
                     <div class="flex flex-col sm:flex-row gap-2">
-                        {{-- <input type="email" name="mail" id="mail"
+                        <input type="email" name="mail" id="mail"
                             class="flex-1 px-4 py-2 text-sm border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent dark:bg-gray-800 dark:text-white transition-colors"
-                            required> --}}
+                            required>
                         <button type="submit"
                             class="px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 rounded-lg transition-colors focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                            super
+                            Subscribe
                         </button>
                     </div>
                 </form>
