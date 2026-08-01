c64ba3a563f6585a471dcd1b172846a70f11551d
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-01 19:30:20 +0500

    redefined the UserController.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Rules\noSpecialChar;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class userController extends Controller
+{
+    function userRegister(Request $request){
+            $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:255', new noSpecialChar],
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'password_confirmation' => 'required|same:password',
+            'terms' => 'accepted',
+        ],[
+            'password_confirmation.same' => 'The confirm password should match the password.',
+            // 'email.unique'=> 'this email already exists, please try another one',
+        ]);
+        
+        User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ]);
+        // User::where('email', $validatedData['email'])->update(['is_verified' => 1]);
+        return view('temps.userwelcome', ['name' => $validatedData['name']]);
+    }
+
+    public function userLogin(Request $request){
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return view('temps.userwelcome', ['name' => Auth::user()->name]);
+        } 
+        else if (!User::where('email', $validatedData['email'])->exists()) {
+            return back()->withErrors(['email' => 'The provided email does not exist.']);
+        }
+        else {
+            return back()->withErrors(['password' => 'The provided password is incorrect.']);
+        }        
+    }
+} 
