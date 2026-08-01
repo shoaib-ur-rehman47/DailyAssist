0e7f4bab5e1e5de563dd3ca1e779d1550810c4f3
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-01 19:27:44 +0500

    removed userController to redefinations.

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::view('/faq', 'generals.faq')->name('faq');
 Route::view('/contact-us', 'generals.contact')->name('contact');
 
 Route::view('/getstarted', 'users.getstarted')->name('getstarted');
-Route::post('/getstarted', [userController::class, "userRegister"]);
 Route::view('/login', 'users.login')->name('login');
+
+Route::post('/getstarted', [userController::class, "userRegister"]);
 Route::post('/login', [userController::class, 'userLogin'])->name('login');
