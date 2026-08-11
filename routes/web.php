c215293b3660bb68fcfea50db020236b5a460efa
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-11 16:12:29 +0500

    learning routing with dynamic perimeters

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{name}', function ($name) {
+    return view('welcome', compact('name'));
 });
