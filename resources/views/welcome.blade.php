454aad53881988017f14818a5c52637217100d2e
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-11 16:23:49 +0500

    set vite css and js, instead of manual linking css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome</title>
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <h1 class="">Welcome {{ $name }}</h1>
+    <h1 class="text-2xl font-bold">Welcome {{ $name }}</h1>
     <h2>nothing</h2>
 </body>
 
