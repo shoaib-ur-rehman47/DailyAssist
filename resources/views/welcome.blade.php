92052dd29d6c03d0313766de199a15b2c41492df
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-12 16:26:08 +0500

    added table to show the data in associative array

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,61 @@
 </head>
 
 <body>
-    @foreach ($students as $student)
-        <p>{{ $student['name'] }} - {{ $student['grade'] }}</p>
-    @endforeach
+    <div class="max-w-4xl mx-auto mt-10 overflow-hidden rounded-xl border border-gray-200 shadow-sm">
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                        ID
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                        Name
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                        Age
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                        Grade
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-100">
+                @forelse ($students as $student)
+                    <tr class="hover:bg-gray-50 transition-colors">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {{ $student['id'] }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {{ $student['name'] }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {{ $student['age'] }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <span @class([
+                                'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-semibold',
+                                'bg-green-100 text-green-800' => $student['grade'] === 'A',
+                                'bg-blue-100 text-blue-800' => $student['grade'] === 'B',
+                                'bg-yellow-100 text-yellow-800' => $student['grade'] === 'C',
+                            ])>
+                                {{ $student['grade'] }}
+                            </span>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="4" class="px-6 py-4 text-center text-sm text-gray-500">
+                            No students found.
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
