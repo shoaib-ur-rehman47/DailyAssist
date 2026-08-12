cfaaace212987f3050eb7e1a909d3ff9dde7c4a0
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-12 16:44:26 +0500

    sorted the associative array by name

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -13,18 +13,19 @@ class WelcomeController extends Controller
     }
 
     public function showdata(){
-$students = [
-    ['id' => 1, 'name' => 'Ayesha Khan', 'age' => 20, 'grade' => 'A'],
-    ['id' => 2, 'name' => 'Bilal Ahmed', 'age' => 21, 'grade' => 'B'],
-    ['id' => 3, 'name' => 'Sana Malik', 'age' => 19, 'grade' => 'A'],
-    ['id' => 4, 'name' => 'Usman Tariq', 'age' => 22, 'grade' => 'C'],
-    ['id' => 5, 'name' => 'Hira Baig', 'age' => 20, 'grade' => 'B'],
-    ['id' => 6, 'name' => 'Ahmed Raza', 'age' => 23, 'grade' => 'A'],
-    ['id' => 7, 'name' => 'Zainab Fatima', 'age' => 19, 'grade' => 'B'],
-    ['id' => 8, 'name' => 'Hassan Ali', 'age' => 21, 'grade' => 'C'],
-    ['id' => 9, 'name' => 'Mahnoor Shah', 'age' => 20, 'grade' => 'A'],
-    ['id' => 10, 'name' => 'Fahad Iqbal', 'age' => 22, 'grade' => 'B'],
-];
+    $students = [
+        ['id' => 1, 'name' => 'Ayesha Khan', 'age' => 20, 'grade' => 'A'],
+        ['id' => 2, 'name' => 'Bilal Ahmed', 'age' => 21, 'grade' => 'B'],
+        ['id' => 3, 'name' => 'Sana Malik', 'age' => 19, 'grade' => 'A'],
+        ['id' => 4, 'name' => 'Usman Tariq', 'age' => 22, 'grade' => 'C'],
+        ['id' => 5, 'name' => 'Hira Baig', 'age' => 20, 'grade' => 'B'],
+        ['id' => 6, 'name' => 'Ahmed Raza', 'age' => 23, 'grade' => 'A'],
+        ['id' => 7, 'name' => 'Zainab Fatima', 'age' => 19, 'grade' => 'B'],
+        ['id' => 8, 'name' => 'Hassan Ali', 'age' => 21, 'grade' => 'C'],
+        ['id' => 9, 'name' => 'Mahnoor Shah', 'age' => 20, 'grade' => 'A'],
+        ['id' => 10, 'name' => 'Fahad Iqbal', 'age' => 22, 'grade' => 'B'],
+    ];
+    usort($students, fn ($a, $b) => strcmp($a['name'], $b['name']));
         return view('welcome', compact('students'));
     }
 
