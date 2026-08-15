9868c3961f21b0ce394e69ea99e0e491cf9790a6
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-15 20:12:22 +0500

    updated the content section height to default.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section("title", "Welcome DailyAssist")
 @section("content")
-    <div class="flex flex-col items-center justify-center min-h-screen">
+    <div class="flex flex-col items-center justify-center bg-gray-100">
         <h1 class="text-4xl font-bold mb-4">Welcome to DailyAssist</h1>
         <p class="text-lg text-gray-600">Your personal assistant for daily tasks.</p>
     </div>
