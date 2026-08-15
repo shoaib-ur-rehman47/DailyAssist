999c806e1075b9f736c3f0809cf6fdaea73eb36e
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-15 20:18:27 +0500

    removed content section.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,2 @@
 @extends('layouts.app')
 @section('title', 'Welcome DailyAssist')
-@section('content')
-    <div class="flex flex-col items-center justify-center bg-gray-100">
-        <h1 class="text-4xl font-bold mb-4">Welcome to DailyAssist</h1>
-        <p class="text-lg text-gray-800 decoration-underline">Welcome to Your personal assistant for daily tasks.</p>
-    </div>
-@endsection
