08f93e83916cf35dc580c6c8e5b9b88abc98b7e1
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-15 20:04:34 +0500

    fixed navbar padding, margin, logo size and weight.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,17 @@
-<nav class=" p-4 flex justify-between items-center">
-    <div class="text-blue-800 logo hover:scale-95 flex gap-0.5"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+<nav class=" px-16 py-4 flex justify-between items-center">
+    <div class="text-blue-800 logo hover:scale-95 flex items-center justify-center font-bold gap-0.5">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round"
                 d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
-        <div class="font-bold text-xl">DailyAssist</div>
+        <div class="font-bold text-2xl">DailyAssist</div>
     </div>
-    <ul class="flex gap-4">
-        <li><a href="#" class="hover:text-gray-700 px-4 py-2">Features</a></li>
-        <li><a href="#" class="hover:text-gray-700 px-4 py-2">How It Works</a></li>
-        <li><a href="#" class="hover:text-gray-700 px-4 py-2">Pricing</a></li>
-        <li><a href="#" class="hover:text-gray-700 px-4 py-2">FAQs</a></li>
+    <ul class="flex">
+        <li><a href="#" class="hover:text-gray-700 px-4 py-2 text-sm">Features</a></li>
+        <li><a href="#" class="hover:text-gray-700 px-4 py-2 text-sm">How It Works</a></li>
+        <li><a href="#" class="hover:text-gray-700 px-4 py-2 text-sm">Pricing</a></li>
+        <li><a href="#" class="hover:text-gray-700 px-4 py-2 text-sm">FAQs</a></li>
     </ul>
     <div>
         <button class="py-2 px-4 hover:bg-gray-200">Log In</button>
