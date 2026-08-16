29adfd924cfc7315e5aebe8a4150b9128f499e73
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-16 20:30:00 +0500

    updated logo and it's link in navbar.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,17 +1,23 @@
 <nav class=" px-16 py-4 flex justify-between items-center border-b-gray-200 border-b-2">
     <div class="text-blue-800 logo hover:scale-95 flex items-center justify-center font-bold gap-0.5">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-        </svg>
-        <div class="font-bold text-2xl">DailyAssist</div>
+        <a href="#" class="flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <div class="font-bold text-2xl cursor-pointer">DailyAssist</div>
+        </a>
     </div>
     <ul class="flex ">
-        <li><a href="#" class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">Features</a></li>
-        <li><a href="#" class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">How It Works</a></li>
-        <li><a href="#" class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">Pricing</a></li>
-        <li><a href="#" class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">FAQ</a></li>
+        <li><a href="#"
+                class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">Features</a></li>
+        <li><a href="#" class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">How It
+                Works</a></li>
+        <li><a href="#"
+                class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">Pricing</a></li>
+        <li><a href="#" class="hover:text-blue-900 hover:border-b-blue-800 hover:border-b-2 px-4 py-2 ">FAQ</a>
+        </li>
     </ul>
     <div>
         <button class="py-1.5  px-4 hover:bg-gray-200 rounded-full">Login</button>
