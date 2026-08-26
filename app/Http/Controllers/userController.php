473dbbd70c6e39efd161371b20566a139f45c0fc
shoaib-ur-rehman47 <47.shoaibguru@gmail.com>
2026-08-26 23:36:20 +0500

    using User model sent the registration data to users table.

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Rules\noSpecialChar;
 use Illuminate\Http\Request;
 
 class getstartedController extends Controller
 {
     function showsignup(Request $request){
-            $request->validate([
+            $validatedData = $request->validate([
             'name' => ['required', 'string', 'max:255', new noSpecialChar],
             'email' => 'required|email',
             'password' => 'required|min:6',
@@ -18,6 +19,11 @@ class getstartedController extends Controller
             'password_confirmation.same' => 'The confirm password should match the password.',
         ]);
         
+        User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ]);
         // return view('temps.userwelcome', ['name' => $request->name]);
         // return $request->all();
     }
